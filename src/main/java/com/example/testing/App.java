0de93cf1dd059d19113142010f1d3406029072ad
Ltl Labs <59998515+ltllabs@users.noreply.github.com>
2022-03-03 13:40:25 +0000

    Fix code smells

--- src/main/java/com/example/testing/App.java
+++ src/main/java/com/example/testing/App.java
@@ -1,8 +1,13 @@
 package com.example.testing;
 
+import java.util.logging.Logger;
+
 public class App {
+  private static final Logger logger = Logger.getLogger(
+      App.class.getName());
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    logger.info("Hello World!");
   }
 
   public static int sum(int a, int b) {
